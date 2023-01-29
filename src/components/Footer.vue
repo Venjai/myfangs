097c845a659438fe494967a1aab42371ef1e7647
Gina van Groen <ginavangroen@gmail.com>
2023-01-29 12:41:05 +0100

    HomePage

---
+++ src/components/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="footer">
+    <div class="upper">
+      <div class="left">
+        <p>
+          <h3>Christian Stegemann</h3>
+          <a href="www.myfangs.de">myfangs.de</a><br />
+          <a href="mailto:vamps66@web.de">E-Mail</a>
+        </p>
+      </div>
+      <div class="right">
+        <ul>
+          <li><a href="#">Impressum</a></li>
+          <li><a href="#">Datenschutz</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="lower">
+      Copyright 2023
+    </div>
+  </div>  
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+  .footer {
+    width: 100%;
+    display: block;
+    position: absolute;
+    bottom: 0;
+  }
+
+  .footer a:hover {
+    color: darkred;
+  }
+
+  .footer .upper {
+    background: #242424;
+    height: 200px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .footer .lower {
+    background: #000;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0px 0px 20px #000;
+  }
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="header">
+    <div class="upper">
+      <div class="logo">Logo</div>
+    </div>
+    <div class="lower">
+      <div class="nav">
+        <ul>
+          <a href="#"><li>Vampirzähne</li></a>
+          <a href="#"><li>Über mich</li></a>
+          <a href="#"><li>Referenzen</li></a>
+          <a href="#"><li>Angebot & Kosten</li></a>
+          <a href="#"><li>Gästebuch</li></a>
+          <a href="#"><li>Kontakt</li></a>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+  .header {
+    width: 100%;
+  }
+
+  .header .upper {
+    height: 80px;
+    background: #000;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0px 0px 20px #000;
+  }
+
+  .header .lower {
+    height: 70px;
+    background: #242424;
+  }
+
+  .header .nav {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+
+  .header .nav ul {
+    display: flex;
+    font-weight: 600;
+  }
+
+  .header .nav ul a {
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    width: 200px;
+    line-height: 35px;
+  }
+
+  
+  .header .nav ul a:hover {
+    background: rgb(113, 21, 11);
+  }
+
+</style>
